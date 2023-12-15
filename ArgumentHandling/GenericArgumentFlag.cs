b9e4eca76ec162b081769639a669c72f5ce9fc3f
thomasmaloney-wk <thomas.maloney@workiva.com>
2023-12-15 17:18:08 -0600

    removed parameters property from generic argument flag as that gets determined after flag registration

--- ArgumentHandling/GenericArgumentFlag.cs
+++ ArgumentHandling/GenericArgumentFlag.cs
@@ -7,13 +7,11 @@ namespace DartSharp.ArgumentHandling
   class GenericArgumentFlag : ArgumentFlag
   {
     public int ParameterCount { get; }
-    public IEnumerable<string> Parameters { get; }
     public override bool IsProcessorFlag => false;
 
-    public GenericArgumentFlag(string flag, string desc, int parameterCount = 0, IEnumerable<string>? parameters = null) : base(flag, desc)
+    public GenericArgumentFlag(string flag, string desc, int parameterCount = 0) : base(flag, desc)
     {
       ParameterCount = parameterCount;
-      Parameters = parameters ?? new List<string>();
     }
   }
 }
