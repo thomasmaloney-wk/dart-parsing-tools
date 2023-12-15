193f21be325b975f1b4107ed3c3544809a12ce5b
thomasmaloney-wk <thomas.maloney@workiva.com>
2023-12-15 17:52:57 -0600

    created IProcessorArgumentFlag interface to allow us to cast ArgumentFlag to processor subtype without having to know the generic processor type ahead of time

--- ArgumentHandling/ProcessorArgumentFlag.cs
+++ ArgumentHandling/ProcessorArgumentFlag.cs
@@ -2,14 +2,20 @@ using DartSharp.Processors;
 
 namespace DartSharp.ArgumentHandling
 {
+    interface IProcessorArgumentFlag
+    {
+        public Type ProcessorType { get; }
+
+        public DartProcessor CreateProcessor(IEnumerable<string> files, string? outputParameter = null);
+    }
     /// <summary>
     /// Represents a program flag associated with a specific <c>DartProcessor</c>.
     /// </summary>
     /// <typeparam name="TProcessor">A subtype of <c>DartProcessor</c></typeparam>
-    class ProcessorArgumentFlag<TProcessor> : ArgumentFlag where TProcessor : DartProcessor
+    class ProcessorArgumentFlag<TProcessor> : ArgumentFlag, IProcessorArgumentFlag where TProcessor : DartProcessor
     {
         public bool UsesOutputFlag { get; }
-        public static Type ProcessorType => typeof(TProcessor);
+        public Type ProcessorType => typeof(TProcessor);
         public override bool IsProcessorFlag => true;
 
         public ProcessorArgumentFlag(string flag, string desc, bool usesOutputFlag = false) : base(flag, desc)
@@ -25,6 +31,11 @@ namespace DartSharp.ArgumentHandling
                 return (TProcessor)Activator.CreateInstance(typeof(TProcessor), files, outputParameter);
             }
             return (TProcessor)Activator.CreateInstance(typeof(TProcessor), files);
+        }
+
+        DartProcessor IProcessorArgumentFlag.CreateProcessor(IEnumerable<string> files, string? outputParameter)
+        {
+            return CreateProcessor(files, outputParameter: outputParameter);
         }
     }
 }
