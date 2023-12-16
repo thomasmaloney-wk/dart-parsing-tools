ff9b671ae033b24905f2276343957608f2fb1cd7
thomasmaloney-wk <thomas.maloney@workiva.com>
2023-12-15 20:42:23 -0600

    added doc comments to IArgumentFlag properties

--- ArgumentHandling/IArgumentFlag.cs
+++ ArgumentHandling/IArgumentFlag.cs
@@ -5,7 +5,18 @@ namespace DartSharp.ArgumentHandling
   /// </summary>
   interface IArgumentFlag
   {
+    /// <summary>
+    /// A string representing the flag passed into the program.
+    /// Ex: "--help"
+    /// </summary>
     public string Flag { get; }
+
+    /// <summary>
+    /// The description of the flag that gets printed when listing
+    /// the available flags.
+    /// 
+    /// Ex: "Prints the help information"
+    /// </summary>
     public string Description { get; }
   }
 }
